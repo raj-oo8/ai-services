e7d73bfae4dd315f303351de73338d5cfa56d0ab
Raj <rj.sse8@outlook.com>
2024-03-30 23:54:55 +0530

    updtaed nugets.

--- Azure.AI.Services.Teams/GlobalSuppressions.cs
+++ Azure.AI.Services.Teams/GlobalSuppressions.cs
@@ -7,3 +7,5 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "ASP0014:Suggest using top level route registrations", Justification = "<Pending>")]
 [assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:Azure.AI.Services.Teams.Search.AISearch.#ctor(Microsoft.Extensions.Configuration.IConfiguration)")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:Azure.AI.Services.Teams.Controllers.BotController.#ctor(Microsoft.Bot.Builder.Integration.AspNet.Core.IBotFrameworkHttpAdapter,Microsoft.Bot.Builder.IBot)")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "<Pending>", Scope = "member", Target = "~M:Azure.AI.Services.Teams.Search.SearchApp.#ctor(Azure.AI.Services.Teams.Search.AISearch)")]
